ba7b8ee6e8b7bb29ab7a2c6be3eec29e393f716b
03-Abhi <9818abhi@gmail.com>
2025-03-30 16:31:57 +0530

    help

--- Source CODE/Scripts/continual/train_continual_linear_chicago.sh
+++ Source CODE/Scripts/continual/train_continual_linear_chicago.sh
@@ -1,8 +1,8 @@
 for seed in {0..10}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "Naive" --dataset "chicago" \
-        --data_dir "./Data/city_payments_encoded_all_new_anomalies.csv" \
+  python main.py --strategy "SynapticIntelligence" --dataset "chicago" \
+        --data_dir "./Data/chicago.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-12.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --seed $seed --training_regime 'continual'
@@ -12,8 +12,8 @@ done
 for seed in {0..10}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "Replay" --dataset "chicago" \
-        --data_dir "./Data/city_payments_encoded_all_new_anomalies.csv" \
+  python main.py --strategy "GEM" --dataset "chicago" \
+        --data_dir "./Data/chicago.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-12.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --replay_mem_size 500 --seed $seed --training_regime 'continual'
@@ -23,8 +23,8 @@ done
 for seed in {0..10}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "EWC" --dataset "chicago" \
-        --data_dir "./Data/city_payments_encoded_all_new_anomalies.csv" \
+  python main.py --strategy "MAS" --dataset "chicago" \
+        --data_dir "./Data/chicago.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-12.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --ewc_lambda 10.0 --seed $seed --training_regime 'continual'
--- Source CODE/Scripts/continual/train_continual_linear_york.sh
+++ Source CODE/Scripts/continual/train_continual_linear_york.sh
@@ -1,8 +1,8 @@
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "SynapticIntelligence" --dataset "philadelphia" \
-        --data_dir "./Data/phil.csv" \
+  python main.py --strategy "SynapticIntelligence" --dataset "york" \
+        --data_dir "./Data/york.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-10.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --seed $seed --training_regime 'continual'
@@ -12,8 +12,8 @@ done
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "GEM" --dataset "philadelphia" \
-        --data_dir "./Data/phil.csv" \
+  python main.py --strategy "GEM" --dataset "york" \
+        --data_dir "./Data/york.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-10.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --replay_mem_size 500 --seed $seed --training_regime 'continual'
@@ -23,8 +23,8 @@ done
 for seed in {0..4}
 do
   echo "Running for seed $seed"
-  python main.py --strategy "MAS" --dataset "philadelphia" \
-        --data_dir "./Data/phil.csv" \
+  python main.py --strategy "MAS" --dataset "york" \
+        --data_dir "./Data/york.csv" \
         --no_epochs 100 --benchmark_configs_path "./BenchmarkConfigs/linear_target-10.yml" \
         --wandb_proj "DeepContinualAuditing" \
         --bottleneck "tanh" --ewc_lambda 10.0 --seed $seed --training_regime 'continual'
